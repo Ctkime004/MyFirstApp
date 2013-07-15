69c49a9620c146f8ac1b3e376e844121383e2956
Corey Kime <corey.kime@gmail.com>
2013-07-14 23:02:24 -0400

    Added simple_form, began work on updating experiment details page, added login/logout to navigation

--- app/models/experiment.rb
+++ app/models/experiment.rb
@@ -1,3 +1,4 @@
 class Experiment < ActiveRecord::Base
-  attr_accessible :createddate, :details, :experimenttype, :startdate, :stopdate, :title, :userid
+  attr_accessible :createddate, :details, :experimenttype, :startdate, :stopdate, :title, :user_id
+  belongs_to :user
 end
