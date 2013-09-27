ce9e378865d7faa547cd5cecbb234d51d605f6d3
Corey Kime <corey.kime@gmail.com>
2013-09-26 21:00:20 -0400

    updated experiment model to delete dependent ad copies if experiment is deleted. Also updated Ad copy form layout

--- app/models/experiment.rb
+++ app/models/experiment.rb
@@ -2,7 +2,7 @@ class Experiment < ActiveRecord::Base
   attr_accessible :details, :experimenttype, :startdate, :stopdate, :title, :user_id
   belongs_to :user
   belongs_to :type, :foreign_key => "experimenttype"
-  has_many :ppc_ad_copies
+  has_many :ppc_ad_copies, :dependent => :destroy
 
   validates :title, presence: true,
   					length: {minimum: 2}
